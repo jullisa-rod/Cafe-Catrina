d5ccbbd303e4c845f2ad1a3d29e519748f9bbcca
Jullisa Rodriguez <jullisa.rodriguez4@gmail.com>
2025-11-14 03:40:05 -0800

    rewriting my home screen and order screen(#3)

--- myfoodshop/src/main/java/com/cafecatrina/HomeScreen.java
+++ myfoodshop/src/main/java/com/cafecatrina/HomeScreen.java
@@ -41,64 +41,6 @@ public class HomeScreen {
 
 
                 }
-
-
-                //switch (Choice) {
-                //case "1":
-                // coffee options
-//                System.out.println("\nChoose your coffee type:");
-//                System.out.println("1. Café de Olla ");
-//                System.out.println("2. Latte ");
-//                System.out.println("3. Macchiato ");
-//                System.out.print("Please make your selection: ");
-//                String coffeeChoice = sc.nextLine();
-//                //sc.nextLine();
-//
-//
-//                System.out.println("Choose size: small / medium / large");
-//                String size = sc.nextLine();
-//
-//                // Flavor
-//                System.out.println("Choose a flavor for your coffee:");
-//                System.out.println("1. Caramel");
-//                System.out.println("2. Vanilla");
-//                System.out.println("3. White Chocolate");
-//                System.out.println("4. Mocha");
-//                System.out.println("5. Masapan (+0.50)");
-//                System.out.println("6. Horchata (+0.50)");
-//                String flavorChoice = sc.nextLine();
-
-
-                // Flavor flavor = createFlavor(flavorChoice);
-
-
-//            String coffeeName = switch (coffeeChoice) {
-//                case 1 -> "Cafe de Olla";
-//                case 2 -> "Latte";
-//                case 3 -> "Macchiato";
-//                default -> "Unknown Coffee";
-//            };
-//
-//            System.out.println("\nPlease select a size:");
-//            System.out.println("1. Small($4.50");
-//            System.out.println("2. Medium ($5.00");
-//            System.out.println("3. Large ($5.50");
-//            System.out.println("Please make your selection:");
-//            int sizeChoice = scanner.nextInt();
-//            scanner.nextLine();
-//
-//
-//            switch (sizeChoice) {
-//                case 1 -> totalPrice = 4.50;
-//                case 2 -> totalPrice = 5.00;
-//                case 3 -> totalPrice = 5.50;
-//                default -> {
-//                    System.out.println("Invalid size!");
-//                    return;
-//                }
-//            }
-//            System.out.println("\nSelect your flavor (type 'done' when finished: ");
-//            System.out.println("Caramel, vanilla, White Chocolate, Mocha, Masapan (+$0.50), Horchata (+$0.50)");
             }
         }
     }
--- myfoodshop/src/main/java/com/cafecatrina/OrderScreen.java
+++ myfoodshop/src/main/java/com/cafecatrina/OrderScreen.java
@@ -7,50 +7,101 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OrderScreen {
-    private static Scanner scanner = new Scanner(System.in);
+
+
+    private static Scanner sc = new Scanner(System.in);
 
     public static void startOrder() {
-        Scanner sc = new Scanner(System.in);
-        boolean running = true;
+
+        boolean ordering = true;
 
 
-        while (running) {
-            System.out.println("Please make your first selection ");
+        while (ordering) {
+            System.out.println("\nPlease make your first selection ");
             System.out.println("c. Add Coffee");
             System.out.println("hc. Add Hot Chocolate");
             System.out.println("p. Add Pan Dulce");
             System.out.println("x. Checkout");
-            System.out.print("Please make your selection: ");
-            String orderChoice = sc.nextLine();
+            System.out.print("choice: ");
+
+            String choice = sc.nextLine().trim().toLowerCase();
+
+            switch (choice) {
+
+                case "c":
+                    handleCoffee();
+                    break;
+
+                case "hc":
+                    System.out.println("Added Hot Chocolate!");
+                    break;
+
+                case "p":
+                    System.out.println("Added Pan Dulce!");
+                    break;
+
+                case "x":
+                    System.out.println("Checking out...");
+                    ordering = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, try again.");
+            }
+        }
+    }
+    private static void handleCoffee() {
+
+        ArrayList<String> coffeeChosen = new ArrayList<>();
+        boolean choosing = true;
 
 
-            boolean coffeechoice = true;
-            while (coffeechoice) {
+            while (choosing) {
 
-                System.out.println("Choose your Coffee Option:");
+                System.out.println("\nChoose your Coffee Option:");
                 System.out.println("l. Latte");
                 System.out.println("m. Macchiato");
-                System.out.println("c. Café de Olla");
-                System.out.println("done choosing");
+                System.out.println("o. Café de Olla");
+                System.out.println("finished choosing");
+
+                System.out.println("Choice:");
+                String coffeeChoice = sc.nextLine().trim().toLowerCase();
+
+
                 double latteCost = 0;
-                boolean choosingCoffee = true;
-                ArrayList<String> coffeeChosen = new ArrayList<>();
+                //boolean choosingCoffee = true;
+                //ArrayList<String> coffeeChosen = new ArrayList<>();
 
-                while (choosingCoffee) {
-                    System.out.println("choice");
-                    String coffeeChoice = scanner.nextLine();
-
-                    switch (coffeeChoice) {
-                        case ("l"):
-                            latteCost += 4.00;
-                            coffeeChosen.add("Latte");
-                            break;
+                switch (coffeeChoice) {
 
 
-                    }
-                }
+                    case "l":
+                        coffeeChosen.add("Latte");
+                        System.out.println("Latte added.");
+                        break;
+
+                    case "m":
+                        coffeeChosen.add("Macchiato");
+                        System.out.println("Macchiato added.");
+                        break;
+
+                    case "o":
+                        coffeeChosen.add("Café de Olla");
+                        System.out.println("Café de Olla added.");
+                        break;
+
+                    default:
+                        System.out.println("Invalid option.");
+
+
+                    //switch (coffeeChoice) {
+                        //case ("l"):
+                            //latteCost += 4.00;
+                           // coffeeChosen.add("Latte");
+                            //break;
 
             }
         }
+        System.out.println("You Selected: " + coffeeChosen);
     }
 }
